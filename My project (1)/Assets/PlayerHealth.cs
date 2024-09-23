19c5ac40be1ec5f98d4875898790dc9c151207f1
PVZPrime <Pvshaw32@gmail.com>
2024-09-23 12:42:40 -0700

    adding scripts

---
+++ My project (1)/Assets/PlayerHealth.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    float health = 10;
+
+    [SerializeField]
+    string leverToLoad = "lose";
+
+    [SerializeField]
+    string GameEnd = "win";
+    float maxHP;
+    [SerializeField]
+    Image healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHP = health;
+        healthBar.fillAmount = health / maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        //IF we hit an enemy, reduce player hp
+        if (collision.gameObject.tag == "Enemy")
+        {
+            healthBar.fillAmount = health / maxHP;
+            health -= 1;
+            // add consequences
+            // If health gets too low, reload the current level
+            if (health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                //SceneManager.LoadScene(levelToLoad);
+            }
+        }
+        if (collision.gameObject.tag == "coin")
+        {
+            SceneManager.LoadScene(GameEnd);
+        }
+    }
+
+    private float GetHealth()
+    {
+        return health;
+    }
+
+
+
+    //IF we run into healer, add restore HP to the player 
+    
+
+}
