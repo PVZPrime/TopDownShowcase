b14ee0791d33ad8e2e8f515184de0cd722e9518d
PVZPrime <Pvshaw32@gmail.com>
2024-09-30 13:59:35 -0700

    added Enemy art

---
+++ TopDownShowcase/Assets/EnemyAnim.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class EnemyAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xInput = GetComponent<Rigidbody2D>().velocity.x;
+        float yInput = GetComponent<Rigidbody2D>().velocity.y;
+        GetComponent<Animator>().SetFloat("x", xInput);
+        GetComponent<Animator>().SetFloat("y", yInput);
+    }
+}
