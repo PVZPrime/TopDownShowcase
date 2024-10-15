effca5f93bdf6cdafab9c9201b45418097c9a4ca
PVZPrime <Pvshaw32@gmail.com>
2024-10-14 22:26:07 -0700

    added main menu

--- TopDownShowcase/Assets/scripts/MainMenu.cs
+++ TopDownShowcase/Assets/scripts/MainMenu.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playGame()
     {
-        
+        Debug.Log("playGame");
+        SceneManager.LoadSceneAsync(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
-    }
-    public void QutiGame()
-    {
-        Debug.Log("Quit");
+        Debug.Log("QuitGame");
         Application.Quit();
-
     }
 }
